9cc6f00529a1a95fc93cb391c8b1a58f426dcab1
Ryan Faircloth <ryan@dss-i.com>
2022-10-13 16:14:26 -0400

    fix: sign

--- main.tf
+++ main.tf
@@ -3,33 +3,6 @@ resource "tls_private_key" "trustanchor_key" {
   ecdsa_curve = "P256"
 }
 
-resource "tls_private_key" "issuer_key" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
-}
-
-resource "tls_cert_request" "issuer_req" {
-  private_key_pem = tls_private_key.issuer_key.private_key_pem
-
-  subject {
-    common_name = "identity.linkerd.cluster.local"
-  }
-}
-
-resource "tls_locally_signed_cert" "issuer_cert" {
-  cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
-  ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
-  ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
-  validity_period_hours = 8760
-  is_ca_certificate     = true
-
-  allowed_uses = [
-    "crl_signing",
-    "cert_signing",
-    "server_auth",
-    "client_auth"
-  ]
-}
 
 resource "tls_self_signed_cert" "trustanchor_cert" {
   private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
@@ -60,8 +33,8 @@ module "argohelm" {
     {
       "k8s-secrets" = {
         "tls" = {
-          "crt" = tls_locally_signed_cert.issuer_cert.cert_pem
-          "key" = tls_private_key.issuer_key.private_key_pem
+          "crt" = tls_self_signed_cert.trustanchor_cert.cert_pem
+          "key" = tls_private_key.trustanchor_key.private_key_pem
         }
       }
     }
