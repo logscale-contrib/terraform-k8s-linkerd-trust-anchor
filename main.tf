3e6b7564d9d8c78c66064517b4345231b8732484
Ryan Faircloth <ryan@dss-i.com>
2022-10-19 09:57:22 -0400

    fix: update chart

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "argohelm" {
   repository       = "ghcr.io/logscale-contrib/helm-linkerd-trust-anchor/charts"
   release          = "cw-trust-anchor"
   chart            = "linkerd2-trust-anchor"
-  chart_version    = "1.1.0"
+  chart_version    = "1.1.1"
   namespace        = "linkerd"
   project          = "cluster-wide"
   create_namespace = false
